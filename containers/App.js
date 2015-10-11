50cb30791ed163153a5c3f3921614989a4d231d5
sliechty <spencerliechty@gmail.com>
2015-10-10 17:40:53 -0700

    Add todo for removing combineReducer

--- containers/App.js
+++ containers/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import Pollard from './Pollard';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import * as reducers from '../reducers';
+import rootReducer from '../reducers/reducer.js';
 
-const reducer = combineReducers(reducers);
+// TODO
+// remove combine reducers call
+// const store = createStore(rootReducer);
+
+debugger;
+const reducer = combineReducers(rootReducer);
 const store = createStore(reducer);
 
 export default class App extends Component {
