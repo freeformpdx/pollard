0eeaa6316993427dfda523ccf768e2fd2665fb01
sliechty <spencerliechty@gmail.com>
2015-10-20 00:05:55 -0700

    Clickable Song gets cursor pointer

--- components/Song.js
+++ components/Song.js
@@ -22,7 +22,7 @@ export default class Song extends Component {
 			<li className="list-group-item clearfix" style={ songStyle }>
 					<div
 						onClick={ (e) => this.handleClick(e) }
-						style={ {marginTop: 5} }
+						style={ {marginTop: 5, cursor:'pointer'} }
 						className="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0">
 						{ this.props.song.inputs.title } - { this.props.song.inputs.artist }
 					</div>
