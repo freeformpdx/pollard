20515aad0105285d7da5235386a30798428276b8
sliechty <spencerliechty@gmail.com>
2015-11-14 17:33:33 -0800

    Slim config && use env vars

--- pollard-template.config.js
+++
@@ -1,6 +0,0 @@
-/* cp to pollard.config.js and fill w/ yr config details */
-export default function config() {
-	return {
-		'socketUrl':''
-	};
-}
--- secrets-template.js
+++
@@ -1,7 +0,0 @@
-/* cp to secrets.js and fill w/ yr api keys as needed */
-export default function secrets() {
-	return {
-		echoNest: '',
-		'7digital': '',
-	};
-}
