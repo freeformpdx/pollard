7dbba5ef483e22736373b404fa2ff7847a909664
sliechty <spencerliechty@gmail.com>
2015-11-15 00:33:55 -0800

    route logging test

--- sprout/routes/index.js
+++ sprout/routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log("**************");
   res.sendfile('./public/index.html');
 });
 
 /* GET home page. */
 router.get('/setlist/', function(req, res, next) {
+	console.log("**************");
   res.sendfile('./public/index.html');
 });
 
