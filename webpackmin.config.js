8178674ea2a02f0db7702f4ca01d46c436fe64a8
sliechty <spencerliechty@gmail.com>
2015-11-14 23:32:09 -0800

    Fix minified build

--- webpackmin.config.js
+++ webpackmin.config.js
@@ -24,6 +24,7 @@ module.exports = {
     }, {
       test: /\.js$/,
       loaders: ['babel'],
+      exclude: /node_modules/,
       include: path.join(__dirname, '..', '..', 'src')
     }, {
       test: /\.css?$/,
