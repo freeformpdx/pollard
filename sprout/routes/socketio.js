654eb6142b0f290ef7651f3dcfda0caeabf29d3e
sliechty <spencerliechty@gmail.com>
2015-11-18 18:51:24 -0800

    Damn, they let me down :(

--- sprout/routes/socketio.js
+++ sprout/routes/socketio.js
@@ -5,15 +5,15 @@ module.exports = function(io) {
 
 	/* GET home page. */
 	router.get('/', function(req, res, next) {
-		res.sendFile('./public/index.html');
+		res.sendfile('./public/index.html');
 	});
 
 	router.get('/advancedSearch', function(req, res, next) {
-		res.sendFile('./public/index.html');
+		res.sendfile('./public/index.html');
 	});
 
 	router.get('/setlist/*', function(req, res, next) {
-		res.sendFile('./public/index.html');
+		res.sendfile('./public/index.html');
 	});
 
 	io.on('connection', function (socket) {
