282f6b4d2c75fda16e3c6eef7fe1226ed3ac2ca2
sliechty <spencerliechty@gmail.com>
2016-04-14 18:33:09 -0700

    man i dont even no nemore

--- .env.js
+++ .env.js
@@ -1,5 +1,5 @@
 module.exports = {
-	SOCKET_URL: process.env.SOCKET_URL || 'localhost',
+	SOCKET_URL: process.env.SOCKET_URL || '192.168.99.100:3420',
 	ECHO_NEST: process.env.ECHO_NEST || '',
 	FMA: process.env.FMA || '',
 	SEVEN_DIGITAL: process.env.SEVEN_DIGITAL || '',
