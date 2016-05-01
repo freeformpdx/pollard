389ec5aa1be5e4d90917f0dc0cf2366a8cb55539
psliechty <spencerliechty@gmail.com>
2016-05-01 13:28:33 -0700

    Add deploy_init sanity check script

---
+++ bin/docker_check_for_running_containers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# used to determine if we can ./bin/deploy_init_sprout.sh
+
+if ! docker top mongo &>/dev/null
+then
+    echo "mongo not running"
+    if ! docker top sprout &>/dev/null
+    then
+        echo "sprout not running"
+        exit 0
+    fi
+fi
+
+echo -e "\nmongo &&/|| sprout is currently running"
+exit 1
