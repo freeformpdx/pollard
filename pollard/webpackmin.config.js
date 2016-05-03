1ba1b0ed9227c297b0a28ee8c63683391fe65144
psliechty <spencerliechty@gmail.com>
2016-05-02 18:04:57 -0700

    does this work?

--- pollard/webpackmin.config.js
+++ pollard/webpackmin.config.js
@@ -14,6 +14,11 @@ module.exports = {
 		new webpack.ProvidePlugin({
 			'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
 		}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"' + process.env.NODE_ENV + '"'
+      }
+    }),
   ],
   module: {
     loaders: [{
