669c66a10b234d5eabf9ebf7c5711e28cafe74bb
psliechty <spencerliechty@gmail.com>
2016-05-10 20:56:13 -0700

    put backups in dir by epoch

--- backup/prod_backup.sh
+++ backup/prod_backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source hosts.env
 
+DATE="$(date +%s)"
+
 echo -e "\n** pushing assets"
 
 deploy/scp_server_assets.sh
@@ -12,6 +14,8 @@ ssh ec2-user@$KFFPPROD \
 
 echo -e "\n** scping backup 2 local"
 
+mkdir -p backup/production/$DOTE
+
 scp -r \
   ec2-user@$KFFPPROD:/data/backup \
-  backup/prod
+  backup/production/$DATE
