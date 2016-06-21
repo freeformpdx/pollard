abc9dd16ff0548dd417323265768f50f7dfd8f73
psliechty <spencerliechty@gmail.com>
2016-06-20 18:58:34 -0700

    typos r 4 n00bz

--- sprout/routes/api.js
+++ sprout/routes/api.js
@@ -40,7 +40,7 @@ router.get('/nowPlaying/:format', cors(), function(req, res, next) {
               break;
             }
           }
-          if (!resopnse) {
+          if (!response) {
             console.log('ERROR: no played songs for setlist ' + setlist.id);
           }
         } else {
