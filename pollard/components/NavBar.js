52b592122b6a49da78cc7fc1ee31cdd2d9102236
psliechty <spencerliechty@gmail.com>
2016-07-02 16:48:07 -0700

    Clean up button attrs

--- pollard/components/NavBar.js
+++ pollard/components/NavBar.js
@@ -26,11 +26,19 @@ class NavBar extends Component {
       <nav className="navbar navbar-inverse navbar-fixed-top">
         <div className="container">
           <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button
+              type="button"
+              className="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#navbar"
+              aria-expanded="false"
+              aria-controls="navbar">
+
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
+
             </button>
             <Link
               id="Home"
